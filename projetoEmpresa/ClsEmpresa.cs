5256e40371951088f2174408fd54e5a3a814ecf8
olgaleticialopes <leticiaolgalopes@gmail.com>
2023-06-03 08:13:23 -0300

    Uma aplicação em windows form utilizando os conceitos basicos de CRUD

---
+++ projetoEmpresa/ClsEmpresa.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace projetoEmpresa
+{
+    class ClsEmpresa
+    {
+        public int ID { get; set; }
+        public string  NOMEDEP { get; set; }
+
+        public string SIGLADEP{ get; set; }
+        Dados objempresa = new Dados();
+
+
+        #region Incluir
+        public void Incluir()
+        {
+            try
+            {
+                if (objempresa.ConvertSqlToInt(
+                    "INSERT INTO TBDEPARTAMENTO(ID, NOMEDEP, SIGLADEP)VALUES(?ID,?NOMEDEP,?SIGLADEP)",
+                    new MySqlParameter("?ID", ID),
+                    new MySqlParameter("?NOMEDEP", NOMEDEP),
+                    new MySqlParameter("?SIGLADEP", SIGLADEP)) != 0)
+
+                    MessageBox.Show("Inclusão realizada");       
+                
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Não foi possivel incluir");
+                
+            }
+        }
+
+        #endregion
+
+
+        #region Consultar
+        public DataTable Consultar()
+        {
+            DataTable tblRecuperada = objempresa.ConvertSqlToDataTable("SELECT * FROM TBDEPARTAMENTO");
+
+            return tblRecuperada;
+        }
+
+        #endregion
+
+
+        #region Alterar
+        public void Alterar()
+        {
+            try
+            {
+
+                if (objempresa.ConvertSqlToInt(
+                  "UPDATE TBDEPARTAMENTO SET ID = ?ID, NOMEDEP = ?NOMEDEP, SIGLADEP = ?SIGLADEP ",
+                  new MySqlParameter("?ID", ID),
+                  new MySqlParameter("?NOMEDEP", NOMEDEP),
+                  new MySqlParameter("?SIGLADEP", SIGLADEP)) != 0)
+
+                MessageBox.Show("Alteração realizada com sucesso!");
+
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("A alteração não pode ser concluida!");
+            }
+        }
+
+        #endregion
+
+
+        #region excluir
+        public void Excluir()
+        {
+            try
+            {
+ 
+             if (objempresa.ConvertSqlToInt("DELETE FROM TBDEPARTAMENTO WHERE ID = ?ID",
+                    new MySqlParameter("?ID", ID)) != 0)
+                     MessageBox.Show("Registro Excluído!");
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Não foi possivel realizar a exclusão! :( ");
+            }
+        }
+        #endregion
+
+
+        #region pesquisar
+        public DataTable Pesquisar()
+        {
+            DataTable tblRecuperada = objempresa.ConvertSqlToDataTable(
+                "SELECT * FROM TBDEPARTAMENTO WHERE ID = ?ID",
+                new MySqlParameter("?ID", ID));
+
+            return tblRecuperada;
+        }
+
+        #endregion
+    }
+}
---
+++ projetoEmpresa/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projetoEmpresa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ClsEmpresa objempresa = new ClsEmpresa();
+
+
+        //recupera todos os dados informados no formulario.
+        private bool RecuperarDados()
+        {
+            try
+            {
+               objempresa.ID = int.Parse(txtCod.Text);
+                objempresa.NOMEDEP = txtNome.Text;
+                objempresa.SIGLADEP = txtSigla.Text;
+                return true;
+            }
+            catch (FormatException) // erro de formato
+            {
+                MessageBox.Show("Preencha todos os campos corretamente!");
+                return false;
+            }
+        }
+
+        private void Redefinir()
+        {
+            txtCod.Clear();
+            txtNome.Clear();
+            txtSigla.Clear();
+           GridEmpresa.DataSource = null;
+        }
+
+        //recuperar
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (RecuperarDados())
+            {
+                objempresa.Incluir();
+                Redefinir();
+                
+            }
+        }
+
+
+        //consultar
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            var DadosEmpresa = objempresa.Consultar();
+
+            if (DadosEmpresa.Rows.Count > 0)
+                GridEmpresa.DataSource = DadosEmpresa;
+            else
+            {
+                MessageBox.Show("Não há registros no banco de dados consultado!");
+                GridEmpresa.Columns.Clear();
+            }
+        }
+
+
+        //alterar
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            if (RecuperarDados())
+            {
+                objempresa.Alterar();
+                Redefinir();
+
+            }
+        }
+
+
+        //excluir
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                objempresa.ID = int.Parse(txtCod.Text);
+                if (MessageBox.Show("Deseja excluir esse registro?",
+                    "Exclusão", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                       objempresa.Excluir();
+                        Redefinir();
+                    }
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Informe o número do Código para a exclusão!");
+                txtCod.Clear();
+            }
+        }
+
+
+        //pesquisar cod
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+           try {
+                objempresa.ID = int.Parse(txtCod.Text);
+
+                var tblDados = objempresa.Pesquisar();
+                txtCod.Text = tblDados.Rows[0]["ID"].ToString();
+                txtNome.Text = tblDados.Rows[0]["NOMEDEP"].ToString();
+                txtSigla.Text = tblDados.Rows[0]["SIGLADEP"].ToString();
+                
+            }
+
+            catch (FormatException)
+            {
+                MessageBox.Show("Digite o Código antes de pesquisar");
+                txtCod.Focus();
+            }
+        }
+    }
+}
