af2c3d1caf06d6250c2d6f13e3bcd01b9b902c9f
olgaleticialopes <leticiaolgalopes@gmail.com>
2023-06-03 08:53:32 -0300

    branch com o código da aula do dia Sat Jun  3 08:53:32     2023

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.cboPara = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtConvertido = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cboDe = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.Blue;
+            this.splitContainer1.Panel1.Controls.Add(this.cboPara);
+            this.splitContainer1.Panel1.Controls.Add(this.label5);
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            this.splitContainer1.Panel1.Controls.Add(this.txtConvertido);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.txtValor);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.cboDe);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackColor = System.Drawing.Color.White;
+            this.splitContainer1.Panel2.Controls.Add(this.button2);
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Size = new System.Drawing.Size(543, 450);
+            this.splitContainer1.SplitterDistance = 366;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // cboPara
+            // 
+            this.cboPara.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboPara.FormattingEnabled = true;
+            this.cboPara.Items.AddRange(new object[] {
+            "Dólar (USD)",
+            "Euro (EUR)",
+            "Peso (ARS) ",
+            "Real (BRL)"});
+            this.cboPara.Location = new System.Drawing.Point(63, 223);
+            this.cboPara.Name = "cboPara";
+            this.cboPara.Size = new System.Drawing.Size(169, 39);
+            this.cboPara.TabIndex = 9;
+            this.cboPara.SelectedIndexChanged += new System.EventHandler(this.cboPara_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(291, 195);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(191, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Valor convertido ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(107, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Para";
+            // 
+            // txtConvertido
+            // 
+            this.txtConvertido.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConvertido.Location = new System.Drawing.Point(296, 223);
+            this.txtConvertido.Name = "txtConvertido";
+            this.txtConvertido.Size = new System.Drawing.Size(169, 38);
+            this.txtConvertido.TabIndex = 6;
+            this.txtConvertido.TextChanged += new System.EventHandler(this.txtTotal_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(300, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "De";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor.Location = new System.Drawing.Point(63, 130);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(169, 38);
+            this.txtValor.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(107, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Valor ";
+            // 
+            // cboDe
+            // 
+            this.cboDe.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboDe.FormattingEnabled = true;
+            this.cboDe.Items.AddRange(new object[] {
+            "Dólar (USD)",
+            "Euro (EUR)",
+            "Peso (ARS) ",
+            "Real (BRL)"});
+            this.cboDe.Location = new System.Drawing.Point(296, 129);
+            this.cboDe.Name = "cboDe";
+            this.cboDe.Size = new System.Drawing.Size(169, 39);
+            this.cboDe.TabIndex = 1;
+            this.cboDe.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(124, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(270, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = ":.Conversor de moedas.:";
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.sair;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Location = new System.Drawing.Point(63, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(33, 34);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.novo;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Location = new System.Drawing.Point(12, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 34);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(543, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboDe;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtConvertido;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cboPara;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTotal_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cboPara_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //calcular
+            
+            if (cboDe.SelectedIndex != -1 && cboPara.SelectedIndex != -1)
+            {
+                try {
+                    double valor, total;
+                    valor = float.Parse(txtValor.Text);
+                    if (cboDe.SelectedIndex == 0 && cboPara.SelectedIndex == 1)
+                    {
+                        //dolar Para euro
+                        total = valor / 1.06;
+                        txtConvertido.Text = $"€ {total}";
+
+                    }
+                    else if (cboDe.SelectedIndex == 0 && cboPara.SelectedIndex == 2)
+                    {
+                        //dolar Para peso
+                        total = valor * 198.26;
+                        txtConvertido.Text = $"$ {total}";
+
+                    }
+                    else if (cboDe.SelectedIndex == 0 && cboPara.SelectedIndex == 3)
+                    {
+                        //dolar Para real
+                        total = valor * 5.20;
+                        txtConvertido.Text = $"R$ {total}";
+
+                    }
+                    else if (cboDe.SelectedIndex == 1 && cboPara.SelectedIndex == 0)
+                    {
+                        //Euro Para dolar
+                        total = valor * 1.06;
+                        txtConvertido.Text = $"$ {total}";
+
+                    }
+                    else if (cboDe.SelectedIndex == 1 && cboPara.SelectedIndex == 2)
+                    {
+                        //Euro Para Peso
+                        total = valor * 198.26;
+                        txtConvertido.Text = $"$ {total}";
+
+                    }
+                    else if (cboDe.SelectedIndex == 1 && cboPara.SelectedIndex == 3)
+                    {
+                        //Euro Para Real
+                        total = valor * 5.51;
+                        txtConvertido.Text = $"R$ {total}";
+
+                    }
+                    else if (cboDe.SelectedIndex == 2 && cboPara.SelectedIndex == 0)
+                    {
+                        //peso Para dolar
+                        total = valor / 198.26;
+                        txtConvertido.Text = $"$ {total}";
+
+                    }
+                    else if (cboDe.SelectedIndex == 2 && cboPara.SelectedIndex == 1)
+                    {
+                        //peso Para Euro
+                        total = valor / 1.06;
+                        txtConvertido.Text = $"€ {total}";
+
+                    }
+                    else if (cboDe.SelectedIndex == 2 && cboPara.SelectedIndex == 3)
+                    {
+                        //PESO PARA REAL
+                        total = valor / 38.1;
+                        txtConvertido.Text = $"R$ {total}";
+                    }
+                    //valores iguais 
+                    else if (cboDe.SelectedIndex == 0 && cboPara.SelectedIndex == 0)
+                    {
+                        if (MessageBox.Show("As moedas são iguais", "Sair", MessageBoxButtons.OK,
+                           MessageBoxIcon.Error) != DialogResult.OK)
+                        {
+
+                            txtValor.Clear();
+                            txtConvertido.Clear();
+                            cboDe.SelectedIndex = -1;
+                            cboPara.SelectedIndex = -1;
+                        }
+                       
+                    }
+                    else if (cboDe.SelectedIndex == 1 && cboPara.SelectedIndex == 1)
+                    {
+                        if (MessageBox.Show("As moedas são iguais", "Sair", MessageBoxButtons.OK,
+                          MessageBoxIcon.Error) != DialogResult.OK)
+                        {
+
+                            txtValor.Clear();
+                            txtConvertido.Clear();
+                            cboDe.SelectedIndex = -1;
+                            cboPara.SelectedIndex = -1;
+                        }
+                    }
+                    else if (cboDe.SelectedIndex == 2 && cboPara.SelectedIndex == 2)
+                    {
+                        if (MessageBox.Show("As moedas são iguais", "Sair", MessageBoxButtons.OK,
+                          MessageBoxIcon.Error) != DialogResult.OK)
+                        {
+
+                            txtValor.Clear();
+                            txtConvertido.Clear();
+                            cboDe.SelectedIndex = -1;
+                            cboPara.SelectedIndex = -1;
+                        }
+                    }
+                    else if (cboDe.SelectedIndex == 3 && cboPara.SelectedIndex == 3)
+                    {
+                        if (MessageBox.Show("As moedas são iguais", "Sair", MessageBoxButtons.OK,
+                             MessageBoxIcon.Error) != DialogResult.OK)
+                        {
+
+                            txtValor.Clear();
+                            txtConvertido.Clear();
+                            cboDe.SelectedIndex = -1;
+                            cboPara.SelectedIndex = -1;
+                        }
+
+                    }
+                }
+                catch (Exception)
+                {
+                    if (MessageBox.Show("Informe os dados corretamente", "Sair", MessageBoxButtons.OK,
+                         MessageBoxIcon.Error) != DialogResult.OK)
+                    {
+                        
+                        
+                        cboDe.SelectedIndex = -1;
+                        cboPara.SelectedIndex = -1;
+                    }
+
+                }
+                
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           //limpar a aplicação
+                txtValor.Clear();
+                txtConvertido.Clear();
+            cboDe.SelectedIndex = -1;
+            cboPara.SelectedIndex = -1;
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //fechar a aplicação
+            if (MessageBox.Show("Desejar encerrar o programa?", "Sair", MessageBoxButtons.YesNo,
+                      MessageBoxIcon.Error) == DialogResult.Yes)
+            {
+                // aplicacao é encerrada
+                Application.Exit();
+            }
+        }
+    }
+}
